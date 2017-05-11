4f5020e92dfddca75006c0bf4ccf68007ef5ecc7
henmerlin <henmerlin@gmail.com>
2017-05-11 12:21:50 -0300

    Ideia Controller

--- bancodeideias/src/java/bancodeideias/controller/routes/IdeiaController.java
+++ bancodeideias/src/java/bancodeideias/controller/routes/IdeiaController.java
@@ -1,7 +1,19 @@
 package bancodeideias.controller.routes;
 
+import bancodeideias.model.entitiy.Ideia;
+import br.com.caelum.vraptor.Controller;
+import javax.faces.bean.RequestScoped;
+
+@Controller
+@RequestScoped
 public class IdeiaController {
+
     public IdeiaController() {
-        super();
+    }
+
+    public void form() {
+    }
+
+    public void add(Ideia i) {
     }
 }
